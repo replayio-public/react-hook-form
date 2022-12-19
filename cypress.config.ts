4011a2461f90150c2144e5c5551f709ff74b6f5e
Jaril <jarilvalenciano@gmail.com>
2022-12-19 14:19:41 -0500

    Dump data into duration.json

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'cypress';
 const cypressReplay = require('@replayio/cypress');
+const fs = require('fs');
 
 export default defineConfig({
   video: false,
@@ -8,6 +9,12 @@ export default defineConfig({
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       cypressReplay.default(on, config);
+      on('after:run', (afterRun: any) => {
+        const data = JSON.stringify(afterRun.totalDuration);
+        const filename = 'duration.json';
+        fs.writeFileSync(filename, data);
+        console.log('cypress-json-results: wrote results to %s', filename);
+      });
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       return config;
     },
