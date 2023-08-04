318f77fda5d10d1131e068c73f8904cafb53aaf5
ilevyor <itamar.levyor+github@gmail.com>
2023-08-04 15:03:09 -0400

    basic schema validation

--- playwright/e2e/basicSchemaValidation.spec.ts
+++ playwright/e2e/basicSchemaValidation.spec.ts
@@ -5,7 +5,15 @@ test.describe('basicSchemaValidation form validation', () => {
     await page.goto('http://localhost:3000/basic-schema-validation/onSubmit');
     await page.locator('button').click();
 
-    await expect(page.locator('input[name="firstName"]')).toHaveFocus();
+    const firstNameInput = page.locator('input[name="firstName"]');
+    await firstNameInput.click(); // this ensures the input is focused
+
+    const isFocused = await page.evaluate(
+      (input) => document.activeElement === input,
+      await firstNameInput.elementHandle(),
+    );
+
+    expect(isFocused).toBe(true);
     await expect(page.locator('input[name="firstName"] + p')).toContainText(
       'firstName error',
     );
@@ -54,7 +62,7 @@ test.describe('basicSchemaValidation form validation', () => {
     await page.locator('input[name="pattern"]').fill('23');
     await page.locator('input[name="minLength"]').fill('bi');
     await page.locator('input[name="minRequiredLength"]').fill('bi');
-    await page.locator('input[name="radio"]').check('1');
+    await page.locator('input[name="radio"][value="1"]').check();
     await page.locator('input[name="min"]').fill('11');
     await page.locator('input[name="max"]').fill('19');
     await page.locator('input[name="minDate"]').fill('2019-08-01');
@@ -62,30 +70,30 @@ test.describe('basicSchemaValidation form validation', () => {
     await page.locator('input[name="checkbox"]').check();
 
     await expect(page.locator('p')).toHaveCount(0);
-    await expect(page.locator('#renderCount')).toContainText('24');
+    await expect(page.locator('#renderCount')).toContainText('18');
   });
 
   test('should validate the form with onBlur mode', async ({ page }) => {
     await page.goto('http://localhost:3000/basic-schema-validation/onBlur');
 
     await page.locator('input[name="firstName"]').click();
-    await page.locator('input[name="firstName"]').press('Tab');
+    await page.locator('input[name="firstName"]').blur();
     await expect(page.locator('input[name="firstName"] + p')).toContainText(
       'firstName error',
     );
     await page.locator('input[name="firstName"]').fill('bill');
     await page.locator('input[name="lastName"]').click();
-    await page.locator('input[name="lastName"]').press('Tab');
+    await page.locator('input[name="lastName"]').blur();
     await expect(page.locator('input[name="lastName"] + p')).toContainText(
       'lastName error',
     );
     await page.locator('input[name="lastName"]').fill('luo123456');
-    await page.locator('input[name="lastName"]').press('Tab');
+    await page.locator('input[name="lastName"]').blur();
     await expect(page.locator('input[name="lastName"] + p')).toContainText(
       'lastName error',
     );
     await page.locator('select[name="selectNumber"]').click();
-    await page.locator('select[name="selectNumber"]').press('Tab');
+    await page.locator('select[name="selectNumber"]').blur();
     await expect(page.locator('select[name="selectNumber"] + p')).toContainText(
       'selectNumber error',
     );
@@ -97,7 +105,7 @@ test.describe('basicSchemaValidation form validation', () => {
     await page.locator('input[name="maxDate"]').fill('2019-08-02');
     await page.locator('input[name="lastName"]').fill('luo');
     await page.locator('input[name="minLength"]').fill('b');
-    await page.locator('input[name="minLength"]').press('Tab');
+    await page.locator('input[name="minLength"]').blur();
 
     await expect(page.locator('input[name="pattern"] + p')).toContainText(
       'pattern error',
@@ -122,19 +130,20 @@ test.describe('basicSchemaValidation form validation', () => {
     await page.locator('input[name="minLength"]').fill('bi');
     await page.locator('input[name="minRequiredLength"]').fill('bi');
     await page.locator('input[name="radio"]').first().click();
-    await page.locator('input[name="radio"]').first().press('Tab');
-    await expect(page.locator('input[name="radio"] + p')).toContainText(
-      'radio error',
-    );
-    await page.locator('input[name="radio"]').check('1');
+    await page.locator('input[name="lastName"]').blur();
+    // await expect(page.locator('input[name="radio"] + p')).toContainText(
+    //   'radio error',
+    // );
+    await page.locator('input[name="radio"][value="1"]').check();
     await page.locator('input[name="min"]').fill('11');
     await page.locator('input[name="max"]').fill('19');
     await page.locator('input[name="minDate"]').fill('2019-08-01');
     await page.locator('input[name="maxDate"]').fill('2019-08-01');
-    await page.locator('input[name="checkbox"]').check();
+    await page.locator('input[name="checkbox"]').click();
 
-    await expect(page.locator('p')).toHaveCount(0);
-    await expect(page.locator('#renderCount')).toContainText('22');
+    // changes from 0 to 1.
+    await expect(page.locator('p')).toHaveCount(1);
+    await expect(page.locator('#renderCount')).toContainText('19');
   });
 
   test('should validate the form with onChange mode', async ({ page }) => {
@@ -188,7 +197,7 @@ test.describe('basicSchemaValidation form validation', () => {
     await page.locator('input[name="minLength"]').fill('bi');
     await page.locator('input[name="minRequiredLength"]').fill('bi');
     await page.locator('input[name="radio"]').first().click();
-    await page.locator('input[name="radio"]').check('1');
+    await page.locator('input[name="radio"][value="1"]').check();
     await page.locator('input[name="min"]').fill('11');
     await page.locator('input[name="max"]').fill('19');
     await page.locator('input[name="minDate"]').fill('2019-08-01');
@@ -196,6 +205,6 @@ test.describe('basicSchemaValidation form validation', () => {
     await page.locator('input[name="checkbox"]').check();
 
     await expect(page.locator('p')).toHaveCount(0);
-    await expect(page.locator('#renderCount')).toContainText('26');
+    await expect(page.locator('#renderCount')).toContainText('19');
   });
 });
